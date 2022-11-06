fce9f26b1de90dfe1f0baf552e6142d08a509c42
Dina Orucevic <dina.oorucevic@gmail.com>
2022-11-06 04:27:37 -0500

    updated script and updated file locations to match flask directory

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
 import os
-from flask import Flask, render_template, request, session, redirect, url_for
 import phonenumbers
-import pickle
-import time
 from multiprocessing import Process, Value
-
-from geopy.geocoders import Nominatim
-import geocoder
-from geopy.distance import geodesic
-from geopy import distance
-
+import time
+import pickle
 from twilio.rest import Client
-
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-users = {"+phonenumber": (36.9659, -78.0950)}
+#twilio account variables
+account_sid = 'ACd990490d592d8154398916f4bab4c306'
+auth_token = '53701984712df9c30edd00b700d537b1'
+client = Client(account_sid, auth_token)
+twilio_phone = '+19896569349'
 
+loc = Nominatim(user_agent="GetLoc")
 
-#twilio client set up
-account_sid = os.environ["xxxx"]
-auth_token  = os.environ["xxxx"]
-client= Client(account_sid, auth_token)
-twilio_phone = "+19896569349"
+#pickle
+users = {}
+pickle_out = open("users.pickle","wb")
+pickle.dump(users, pickle_out)
+pickle_out.close()
 
-location = Nominatim(user_agent="GetLoc")
-
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def form():
+    global users
+    users = readPickleDict(users)
     if request.method == 'POST':
         phone = request.form.get('phone')
+        val_phone = validate_phone(phone)
         destination = request.form.get('destination')
-        destination_location = location.geocode(destination)
-        coords = (destination_location.latitude, destination_location.longitude)
-        users = {phone: coords}        
-    return render_template('welcome.html')
+        destination_loc = loc.geocode(destination)
+        if(val_phone is not None and destination_loc is not None):
+            coords = (destination_loc.latitude, destination_loc.longitude)
+            values = [coords,True]
+            users[val_phone] = values
+            writePickleDict(users)
+            return render_template('welcome.html')
+        elif(val_phone is None):
+            return render_template('signin.html', msg="Invalid phone.")
+        elif(destination_loc is None):
+            return render_template('signin.html', msg="Invalid")
+    return render_template('signin.html', msg=None)
+
+def validate_phone(phone):
+    try:
+        p = phonenumbers.parse(phone,"US")
+        if not phonenumbers.is_valid_number(p):
+            p = "+1" + str(p)
+            p = phonenumbers.parse(phone,"US")
+        if not phonenumbers.is_valid_number(p):
+            return None
+        return phonenumbers.format_number(p, phonenumbers.PhoneNumberFormat.E164)
+    except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
+        return None
 
 @app.route('/success', methods=['GET', 'POST'])
 def success():
@@ -42,30 +63,44 @@ def success():
     return render_template('welcome.html')
 
 def message(phone):
-    message = client.messages.create(body="Hello from python", to="phone", from_="twilio_phone")
+    message = client.messages.create(
+                        to=phone,
+                        from_=twilio_phone,
+                        body='This is your wake up call from eazzZy sleep! You are approximately 5 minutes away from your destination!🌛🌟' )
 
-def track(phone, destination, threshold):
-    current_location = geocoder.ip('me')
-    current_coordinates = (38.9659, -78.0950)
-    if geodesic(current_coordinates, destination).miles <= 5: #logic for user being close to destination
+def writePickleDict(x):
+    pickle_out = open("users.pickle","wb")
+    pickle.dump(x, pickle_out)
+    pickle_out.close()
+
+def readPickleDict(x):
+    pickle_in = open("users.pickle","rb")
+    x = pickle.load(pickle_in)
+    return x
+
+def track(phone, destination):
+    f = open("location.txt", "r")
+    longitude = f.readline()
+    latitude = f.readline()
+    curr_coords = (longitude, latitude)
+    if geodesic(curr_coords, destination) <= 7: #if they're within the threshold of 7km, text user
+        print("RECEIVING TEXT MESSAGE SOON ... !!!")
         message(phone)
-        del users[phone]
-        print("time to wake up!")
-
-def main_loop():
+        users[phone][2] = False
+        writePickleDict(users)
+        
+def output():
     while True:
-        for a, v in users:
-            phone = a
-            destination = v[0]
-            threshold = 5
-            track(phone, destination, threshold)
-
+        global users
+        users = readPickleDict(users)
+        for user in users:
+            if users[user][1]:
+                destination = users[user][0]
+                print("tracking on bus to " + str(destination) + "\n")
+                track(user, destination)
+        time.sleep(3) #refresh happens every 3 seconds
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    print("start")
-    recording_on = Value('b', True)
-    p = Process(target=main_loop)
-    p.start()
+    process = Process(target=output)
+    process.start()  
     app.run(debug=True, use_reloader=False)
-    p.join()
